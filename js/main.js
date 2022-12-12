25da3bdd9825fa504fa1761b75ae891b8ade2ea1
ibrahim <ibrahimmdoukhpc@gmail.com>
2022-12-12 23:05:17 +0300

    inthilize

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 6,
+    spaceBetween: 49,
+    slidesPerGroup: 2,
+    loopFillGroupWithBlank: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
